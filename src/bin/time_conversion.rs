dd6431b11ce9d6480cee715ac03f3d2efc8364b3
Brahim-Amrouche <amroucheb4@gmail.com>
2024-10-01 16:21:57 +0100

    time conversion

---
+++ src/bin/time_conversion.rs
@@ -0,0 +1,26 @@
+use core::time;
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let s = stdin_iterator.next().unwrap().unwrap();
+
+    let result = timeConversion(&s);
+
+    writeln!(&mut fptr, "{}", result).ok();
+}
